5015b13d2441eb5d5124ee4506ac434ade0dac8a
uzielhp <uzielhp@gmail.com>
2026-05-11 04:52:08 +0000

    .

--- principal.js
+++ principal.js
@@ -3,7 +3,7 @@ class MyNavbar extends HTMLElement {
     this.innerHTML = `
       <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-          <a class="navbar-brand" href="index.html"><img src="logo.png" alt="Logo" height="26px"></a>
+          <a class="navbar-brand" href="index.html"><img src="Logo.png" alt="Logo" height="26px"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
